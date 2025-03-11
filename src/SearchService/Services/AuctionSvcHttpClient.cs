a4841af53b28bb6941037f7a830c74d30521d0b5
Searedphantasm <searedphantasm@gmail.com>
2025-03-11 06:47:11 -0700

    Adding Sync Communication between Services

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionSvcHttpClient
+{
+    private readonly HttpClient _client;
+    private readonly IConfiguration _config;
+
+    public AuctionSvcHttpClient(HttpClient client, IConfiguration config)
+    {
+        _client = client;
+        _config = config;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(y => y.UpdatedAt)).Project(z => z.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+        
+        return await _client.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+    }
+}
