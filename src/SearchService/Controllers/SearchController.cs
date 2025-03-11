81b3e1da79f40eccb94ca3ab75d354f82877a8d9
Searedphantasm <searedphantasm@gmail.com>
2025-03-11 06:22:12 -0700

    Creating Search Controller and SearchParams

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHeplers;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    // we don't need to inject anything
+
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
+    {
+        var query = DB.PagedSearch<Item, Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+        {
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
+
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)), // Auction ending soonest
+        };
+
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) 
+                                             && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow) // Default filterBy
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+        
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+        
+        query.PageNumber(searchParams.Page)
+            .PageSize(searchParams.PageSize);
+
+        var results = await query.ExecuteAsync();
+        
+        return Ok(new {
+            results = results.Results,
+            pageCount = results.PageCount,
+            totalCount = results.TotalCount,
+        });
+    }
+}
